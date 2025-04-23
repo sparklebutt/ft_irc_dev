7e14782da19af20c5b8ae9512d0c18e271c937ca
Jack Waddington <jack@waddington.me>
2025-04-23 15:06:58 +0300

    exams

---
+++ exam05/42-School-Exam-Rank-05/20250423/cpp_module_01/Warlock.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include "ASpell.hpp"
+
+class Warlock{
+    private:
+        Warlock & operator=(Warlock const &rhs);
+        Warlock(Warlock const &obj);
+        Warlock();
+        std::string _name;
+        std::string _title;
+        std::map < std::string, ASpell * > _SpellBook;
+    public:
+        Warlock(const std::string& name, const std::string& title);
+        ~Warlock();
+        std::string const &getName() const;
+        std::string const &getTitle() const;
+        void setTitle(std::string const &str);
+        void introduce() const;
+        void learnSpell(ASpell* spell);
+        void forgetSpell(std::string SpellName);
+        void launchSpell(std::string SpellName, ATarget const &target);
+  };
