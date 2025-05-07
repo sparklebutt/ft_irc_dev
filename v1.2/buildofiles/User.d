f2567ab1a55c782b50dd269ed4ad9836757dfc5a
sparklebutt <arandomsanti@gmail.com>
2025-05-07 09:10:14 +0300

    chnages made to try multiple clients

--- v1.2/buildofiles/User.d
+++
@@ -1,8 +0,0 @@
-buildofiles/User.o: sources/User.cpp includes/User.hpp \
- includes/Server.hpp includes/ServerError.hpp includes/config.h \
- includes/SendException.hpp
-includes/User.hpp:
-includes/Server.hpp:
-includes/ServerError.hpp:
-includes/config.h:
-includes/SendException.hpp:
--- v1.2/buildofiles/epoll_utills.d
+++
@@ -1,8 +0,0 @@
-buildofiles/epoll_utills.o: sources/epoll_utills.cpp \
- includes/epoll_utils.hpp includes/Server.hpp includes/ServerError.hpp \
- includes/config.h includes/signal_handler.h
-includes/epoll_utils.hpp:
-includes/Server.hpp:
-includes/ServerError.hpp:
-includes/config.h:
-includes/signal_handler.h:
